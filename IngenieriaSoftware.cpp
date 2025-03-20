6b1eff4ce15363e1c889e2fd67a8bc4b62cbdb46
luciafs9 <44375776+luciafs9@users.noreply.github.com>
2025-03-20 02:14:48 +0100

    Practica clase State Machine

---
+++ IngenieriaSoftware.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <unordered_map>
+#include <functional>
+#include <windows.h>
+
+#pragma comment(lib,"user32")
+#define GetKey(X) (!!(GetAsyncKeyState(0[#X])&0x8000))
+
+using namespace std;
+
+enum class State {
+  STANDING,
+  DUCKING,
+  JUMPING,
+  DIVING
+};
+
+class StateMachine {
+private:
+  State currentState;
+
+public:
+  StateMachine() : currentState(State::STANDING) {}
+
+  void TransitionsStateMachine() {
+    if (currentState == State::STANDING) 
+    {
+      if (GetKey(A)) 
+      {
+        currentState = State::DUCKING;
+      }
+      else if(GetKey(B))
+      {
+        currentState = State::JUMPING;
+      }
+    }
+    else if(currentState == State::DUCKING) 
+    {
+      if (!GetKey(A)) 
+      {
+        currentState = State::STANDING;
+      }
+    }
+    else if (currentState == State::JUMPING) 
+    {
+      if (GetKey(B)) 
+      {
+        currentState = State::DIVING;
+      }
+    }
+  }
+
+  void PrintStates() {
+    switch (currentState) {
+    case State::STANDING: 
+      cout << "Standing" << endl; 
+      break;
+    case State::DUCKING: 
+      cout << "Ducking" << endl;
+      break;
+    case State::JUMPING: 
+      cout << "Jumping" << endl;
+      break;
+    case State::DIVING: 
+      cout << "Diving" << endl;
+      break;
+    }
+  }
+};
+
+int main() {
+  StateMachine sm;
+
+  while (true) {
+    sm.TransitionsStateMachine();
+    sm.PrintStates();
+    Sleep(100);
+  }
+
+  return 0;
+}
